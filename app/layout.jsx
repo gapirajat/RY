40a0f0f259cd0bbe12cb3f91a1fab421a9450409
gapirajat <rajatshinde01@gmail.com>
2024-08-17 14:50:48 +0530

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <head>
+      <Head>
       <link rel="icon" href="icon.png" sizes="any" />
-      </head>
+      </Head>
     </html>
   );
 }
